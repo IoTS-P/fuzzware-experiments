5ad88df9c4fc3b3e5a6a9b4eff3fdb4266b805b6
Ruibo Lu <reaperlu@hust.edu.cn>
2024-07-07 15:20:26 +0800

    [contiki-ng]: add realworld build script for CVE-2019-9183
    Signed-off-by: Ruibo Lu <reaperlu@hust.edu.cn>

--- 03-fuzzing-new-targets/contiki-ng/building/12140-cc2650.sh
+++ 03-fuzzing-new-targets/contiki-ng/building/12140-cc2650.sh
@@ -3,8 +3,8 @@ DIR="$(dirname "$(readlink -f "$0")")"
 
 # This script is to be run within the contiki docker container
 
-CVENUM=2020-12140-real
-SAMPLE_DIR=/workdir/contiki-ng/examples/hello-world
+CVENUM=2020-12140-cc2650
+SAMPLE_DIR=/workdir/contiki-ng/examples/platform-specific/cc26x0-cc13x0/ble-ipv6
 
 cd /workdir/contiki-ng
 # Restore git state
@@ -18,24 +18,21 @@ git checkout release/v4.4
 #git apply $DIR/patches/cc2538_read.patch
 
 # configure l2cap in hello-world sample
-git apply $DIR/patches/l2cap_sample.patch
+#git apply $DIR/patches/l2cap_sample.patch
 
 # bugs in target identified after paper release, patch
 # git apply $DIR/patches/fix-l2cap-issues.patch
 
-# test 
-git apply $DIR/patches/cc2538_norom.patch
-
 # patches for hardfault
-git apply $DIR/patches/12140-hardfault.patch
-git apply $DIR/patches/log.patch
-git apply $DIR/patches/packet_ptr.patch
-git apply $DIR/patches/no-dma.patch
+#git apply $DIR/patches/12140-hardfault.patch
+#git apply $DIR/patches/log.patch
+#git apply $DIR/patches/packet_ptr.patch
+#git apply $DIR/patches/no-dma.patch
 
-make -C $SAMPLE_DIR TARGET=cc2538dk distclean all
+make -C $SAMPLE_DIR TARGET=cc26x0-cc13x0 BOARD=launchpad/cc2650 distclean all
 
 # Copy sample to outside-visible directory
 OUT_DIR=/workdir/rebuilt/CVE-$CVENUM
 rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
-cp $SAMPLE_DIR/build/cc2538dk/hello-world.bin $SAMPLE_DIR/build/cc2538dk/hello-world.elf $OUT_DIR
+cp $SAMPLE_DIR/build/cc2538dk/* $OUT_DIR
